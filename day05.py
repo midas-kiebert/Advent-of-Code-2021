1b6d47ac25ff434d194c49dd82b961e2b9fb1daf
Midas Kiebert <midascubert@gmail.com>
2021-12-12 14:58:50 +0100

    cleanup

--- day05.py
+++ day05.py
@@ -10,20 +10,24 @@ vertical = [line for line in lines if line[0] == line[2]]
 horizontal = [line for line in lines if line[1] == line[3]]
 diagonal = [line for line in lines if line not in vertical and line not in horizontal]
 
+# Returns every number between a & b inclusive, also works for b > a
+def between(a, b):
+    return list(range(a, b + np.sign(b - a), np.sign(b - a)))
+
 # Add 1 to every coordinate the line crosses
 for x1, y1, _, y2 in vertical:
-    for i in range(y1, y2 + np.sign(y2 - y1), np.sign(y2 - y1)):
-        board[i][x1] += 1
+    for y in between(y1, y2):
+        board[y][x1] += 1
 
 for x1, y1, x2, _ in horizontal:
-    for i in range(x1, x2 + np.sign(x2 - x1), np.sign(x2 - x1)):
-        board[y1][i] += 1
+    for x in between(x1, x2):
+        board[y1][x] += 1
 
 print(np.count_nonzero(board >= 2))
 
 # Uses range that is inclusive and unordered
 for x1, y1, x2, y2 in diagonal:
-    for i, j in enumerate(range(x1, x2 + np.sign(x2 - x1), np.sign(x2 - x1))):
-        board[y1 + i * np.sign(y2 - y1)][j] += 1
+    for i, x in enumerate(between(x1, x2)):
+        board[y1 + i * np.sign(y2 - y1)][x] += 1
 
 print(np.count_nonzero(board >= 2))
