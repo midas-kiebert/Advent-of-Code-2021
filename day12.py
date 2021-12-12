70199610cbea2d0eba03fb542ae0fcf4b74ac590
Midas Kiebert <midascubert@gmail.com>
2021-12-12 09:36:47 +0100

    comments

--- day12.py
+++ day12.py
@@ -1,29 +1,42 @@
 from collections import defaultdict
 
 lines = [line.strip().split('-') for line in open('inputs/day12.txt')]
-lookup_table = defaultdict(list)
 
+# Create a dictionary to store the neighbouring nodes of each node
+neighbours = defaultdict(set)
 for line in lines:
-    lookup_table[line[0]].append(line[1])
-    lookup_table[line[1]].append(line[0])
+    neighbours[line[0]].add(line[1])
+    neighbours[line[1]].add(line[0])
 
-def search(current, visited, has_visited_twice):
+# Recursive depth first search, each time 'end' is reached increment the total and return
+# Only continue the search if node is uppercase or if it has not yet been visited
+def search(current, visited):
     global total
     if current == 'end':
         total += 1
         return
-    for node in lookup_table[current]:
-        if node.isupper():
-            search(node, visited, has_visited_twice)
-        elif node not in visited:
-            search(node, visited | {node}, has_visited_twice)
-        elif has_visited_twice == False and node not in {'start', 'end'}:
-            search(node, visited, True)
+    for node in neighbours[current]:
+        if node.isupper() or node not in visited:
+            search(node, visited | {node})
+
+# If the node is uppercase or has not yet been visited, continue like normal
+# If the node is lowercase and has been visited, the search after that node will
+# be exactly the same as if it was part 1, so switch over to the part 1 function
+def search2(current, visited):
+    global total
+    if current == 'end':
+        total += 1
+        return
+    for node in neighbours[current]:
+        if node.isupper() or node not in visited:
+            search2(node, visited | {node})
+        elif node not in {'start', 'end'}:
+            search(node, visited)
 
 total = 0
-search('start', {'start'}, True)
+search('start', {'start'})
 print(total)
 
 total = 0
-search('start', {'start'}, False)
+search2('start', {'start'})
 print(total)
