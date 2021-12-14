7d43c77d4645edf0dc36fe68b1ac8ed50d6a1460
Midas Kiebert <midascubert@gmail.com>
2021-12-14 08:52:03 +0100

    day 14 completed

--- day14.py
+++ day14.py
@@ -5,16 +5,12 @@ input = [line.strip() for line in open('inputs/day14.txt')]
 
 initial = input[0]
 ins = [line.split(" -> ") for line in input[2:]]
-instructs = defaultdict(str)
-for i in ins:
-    instructs[i[0]] = i[1]
-letters = set()
-for i in initial:
-    letters.add(i)
-for i in ins:
-    letters.add(i[1])
 
 # OLD SLOW:
+# instructs = defaultdict(str)
+# for i in ins:
+#     instructs[i[0]] = i[1]
+
 # def iterate(str, instructs):
 #     insertions = []
 #     for i in range(len(str) - 1):
@@ -31,17 +27,17 @@ for i in ins:
 # for i in range(10):
 #     str = iterate(str, instructs)
 #     counts = set()
-#     for a in letters:
+#     for a in str:
 #         counts.add(str.count(a))
 
-def iterate_fast(instructs, tally, ctr):
+def iterate_fast(ins, tally, ctr):
     new_tally = defaultdict(int)
-    for i in instructs:
-        new_1 = i[0] + instructs[i]
-        new_2 = instructs[i] + i[1]
-        new_tally[new_1] += tally[i]
-        new_tally[new_2] += tally[i]
-        ctr[instructs[i]] += tally[i]
+    for i in ins:
+        new_1 = i[0][0] + i[1]
+        new_2 = i[1] + i[0][1]
+        new_tally[new_1] += tally[i[0]]
+        new_tally[new_2] += tally[i[0]]
+        ctr[i[1]] += tally[i[0]]
     return new_tally
 
 str = initial
@@ -56,7 +52,7 @@ for let in str:
 
 tally2 = tally
 for i in range(10):
-    tally2 = iterate_fast(instructs, tally2, ctr)
+    tally2 = iterate_fast(ins, tally2, ctr)
 
 counts = []
 for i in ctr:
@@ -65,7 +61,7 @@ for i in ctr:
 print(max(counts) - min(counts))
 
 for i in range(40):
-    tally = iterate_fast(instructs, tally, ctr)
+    tally = iterate_fast(ins, tally, ctr)
 
 counts = []
 for i in ctr:
