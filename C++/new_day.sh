aedb6aba55502077d6f1748441128dde31b4610a
Midas Kiebert <midascubert@gmail.com>
2022-01-12 21:54:24 +0100

    automated file creation

---
+++ C++/new_day.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+YEAR="2021"
+COOKIE=$AOCSESSION
+
+ZERO="0"
+if [ $1 -ge 10 ]; then
+    ZERO=""
+fi
+
+DAY=day$ZERO$1
+
+FILE=$DAY.cpp
+if test -f "$FILE"; then
+    exit
+fi
+
+cp template.cpp $FILE
+sed -i 's/dayXX/'$DAY'/g' $FILE
+
+curl --silent -b $COOKIE https://adventofcode.com/$YEAR/day/$1/input -o ../inputs/$DAY.txt
+
+if grep -q $DAY "Makefile"; then
+    exit
+fi
+
+echo -e "\n$DAY: $FILE\n\tg++ -o $DAY $FILE\n" >> Makefile
+sed -i '1{s/$/ '$DAY'/}' Makefile
+sed -i '$ d' ../inputs/$DAY.txt
