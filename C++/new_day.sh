8dc589c00b6a9d6f00c3e33446bd0e29f8a243d6
Midas Kiebert <midascubert@gmail.com>
2022-01-12 21:57:09 +0100

    cleanup

--- C++/new_day.sh
+++ C++/new_day.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 YEAR="2021"
-COOKIE=$AOCSESSION
 
 ZERO="0"
 if [ $1 -ge 10 ]; then
@@ -18,7 +17,7 @@ fi
 cp template.cpp $FILE
 sed -i 's/dayXX/'$DAY'/g' $FILE
 
-curl --silent -b $COOKIE https://adventofcode.com/$YEAR/day/$1/input -o ../inputs/$DAY.txt
+curl --silent -b $AOCSESSION https://adventofcode.com/$YEAR/day/$1/input -o ../inputs/$DAY.txt
 
 if grep -q $DAY "Makefile"; then
     exit
