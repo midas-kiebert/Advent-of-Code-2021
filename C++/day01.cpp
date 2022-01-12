32ae38720ffbde2dcfed2728b2785eebe6f695db
Midas Kiebert <midascubert@gmail.com>
2022-01-12 20:06:19 +0100

    cleanup

--- C++/day01.cpp
+++ C++/day01.cpp
@@ -47,12 +47,12 @@ int main() {
     auto start1 = chrono::high_resolution_clock::now();
     auto ans1 = part1(input);
     auto end1 = chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = (end1 - start1) * 1000;
+    chrono::duration<double> elapsed = (end1 - start1) * 1000;
 
     auto start2 = chrono::high_resolution_clock::now();
     auto ans2 = part2(input);
     auto end2 = chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed2 = (end2 - start2) * 1000;
+    chrono::duration<double> elapsed2 = (end2 - start2) * 1000;
 
     int w = max(to_string(ans1).length(), to_string(ans2).length());
     cout << setw(w) << ans1 << setw(20) << "Solved in " << elapsed.count() << " ms\n";
