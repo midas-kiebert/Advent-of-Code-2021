007a6c07260309b05dcb0af316cf9edc608c1e43
Midas Kiebert <midascubert@gmail.com>
2022-01-18 02:26:29 +0100

    format

--- Haskell/day02.hs
+++ Haskell/day02.hs
@@ -1,18 +1,32 @@
 import Data.DList (list)
 readInput :: FilePath -> IO [[String]]
-readInput path = map words . lines <$> readFile path
+readInput path
+    = map words
+    . lines <$> readFile path
 
 moves :: String -> [[String]] -> [Int]
-moves direction input = [if head x == direction then read $ last x else 0 | x <- input]
+moves direction input
+    = [if head x == direction then
+            read $ last x
+       else
+           0
+       | x <- input]
 
 part1 :: [[String]] -> Int
-part1 input = sum (moves "forward" input) * (sum (moves "down" input) - sum (moves "up" input))
+part1 input
+    = sum (moves "forward" input)
+    * (sum (moves "down" input)
+    - sum (moves "up" input))
 
 aims :: [[String]] -> [Int]
-aims input = scanl1 (+) $ zipWith (-) (moves "down" input) (moves "up" input)
+aims input
+    = scanl1 (+)
+    $ zipWith (-) (moves "down" input) (moves "up" input)
 
 part2 :: [[String]] -> Int
-part2 input = sum (moves "forward" input) * sum (zipWith (*) (moves "forward" input) (aims input))
+part2 input
+    = sum (moves "forward" input)
+    * sum (zipWith (*) (moves "forward" input) (aims input))
 
 main :: IO ()
 main = do
